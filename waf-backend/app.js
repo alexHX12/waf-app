dfec20ea72c97a8fd829b5c34370990837490e35
Alex Della Bruna <alexdellabrunadev@gmail.com>
2022-03-25 23:48:07 +0100

    Minor fixes

--- waf-backend/app.js
+++ waf-backend/app.js
@@ -3,11 +3,26 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
+
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://dev-fmeenf3n.us.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'http://localhost:8080',
+  issuer: 'https://dev-fmeenf3n.us.auth0.com/',
+  algorithms: ['RS256']
+});
 
 var rootRouter = require('./routes/root');
 
 var app = express();
 
+app.use(jwtCheck);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -15,20 +30,23 @@ app.use(cookieParser());
 
 app.use('/', rootRouter);
 
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500).json({
+        message: err.message,
+        error: err
+    });
 });
 
 module.exports = app;
