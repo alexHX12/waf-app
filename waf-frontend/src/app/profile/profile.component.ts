6ce8cc8ea4d0b2765e4b5ff736b8639a1b52129a
Alex Della Bruna <alexdellabrunadev@gmail.com>
2022-03-28 23:08:49 +0200

    Added password reset

--- waf-frontend/src/app/profile/profile.component.ts
+++ waf-frontend/src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile',
@@ -8,9 +9,20 @@ import { AuthService } from '@auth0/auth0-angular';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor(public auth: AuthService) { }
+  constructor(public auth: AuthService, private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  changePassword() {
+    this.auth.user$.subscribe((user) => {
+      this.http.post("https://dev-fmeenf3n.us.auth0.com/dbconnections/change_password", {
+        client_id: 'PQuVo6cmELKpqSWA8FhuQinoREXfufWU',
+        email: user?.email,
+        connection: 'Username-Password-Authentication'
+      }, { headers: { 'content-type': 'application/json'},responseType:"text"}).subscribe((res)=>{
+        console.log(res);
+      })
+    });
+  }
 }
