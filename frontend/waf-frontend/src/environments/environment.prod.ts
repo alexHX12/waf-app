4f5419eeefd25007bd276610c7c30b7f5d6dcd60
Alex Della Bruna <alexdellabrunadev@gmail.com>
2022-07-07 18:27:02 +0200

    Production fixes 4

--- frontend/waf-frontend/src/environments/environment.prod.ts
+++ frontend/waf-frontend/src/environments/environment.prod.ts
@@ -1,17 +1,17 @@
 export const environment = {
   production: true,
-  backendURL: "http://api.waf-1.ml",
-  localURL: "http://gestione.waf-1.ml",
+  backendURL: "https://api.waf-1.ml",
+  localURL: "https://gestione.waf-1.ml",
   clientID:{
     domain: 'waf-tenant.us.auth0.com',
     clientId: '3GDnYnEvCmLR5p3z1cMJtYhJQldVlr43',
-    audience:'http://api.waf-1.ml',
+    audience:'https://api.waf-1.ml',
     httpInterceptor: {
       allowedList: [
         {
-          uri: 'http://api.waf-1.ml/*',
+          uri: 'https://api.waf-1.ml/*',
           tokenOptions: {
-            audience: 'http://api.waf-1.ml'
+            audience: 'https://api.waf-1.ml'
           }
         }
       ]
