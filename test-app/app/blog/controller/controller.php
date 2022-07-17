54d90ff297dc15e33fcfd2ae7f74065b81abd092
Alex Della Bruna <alexdellabrunadev@gmail.com>
2022-07-17 17:04:57 +0200

    Minor fixes 6

--- test-app/app/blog/controller/controller.php
+++ test-app/app/blog/controller/controller.php
@@ -5,8 +5,8 @@ class Controller
 {
     public static function createView($name)
     {
-        require_once("./views/navbar.php");
         Login::checkLogin();
+        require_once("./views/navbar.php");
         if (!isset($_SESSION['email'])) {
             require_once("./views/login.php");
         }
