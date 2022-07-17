d2abaa90345ad864b72a2c22fbc68ac33784a5b0
Alex Della Bruna <alexdellabrunadev@gmail.com>
2022-07-17 17:59:38 +0200

    Minor fixes 8

--- test-app/app/blog/controller/controller.php
+++ test-app/app/blog/controller/controller.php
@@ -1,11 +1,11 @@
 <?php
 require_once("controller/login.php");
+require_once('./controller/dbConnection.php');
 
 class Controller
 {
     public static function createView($name)
     {
-        require_once('./controller/dbConnection.php');
         Login::checkLogin();
         require_once("./views/navbar.php");
         if (!isset($_SESSION['email'])) {
--- test-app/app/blog/routes.php
+++ test-app/app/blog/routes.php
@@ -4,27 +4,27 @@ require_once("controller/newpost.php");
 require_once("controller/post.php");
 
 Route::set('index.php',function(){
-    Post::createView('post');
     if (isset($_SESSION['email'])) {
         Post::newComment();
         Post::deletePost();
         Post::deleteComment();
     }
+    Post::createView('post');
 });
 Route::set('newpost',function(){
     if (isset($_SESSION['email'])) {
-        NewPost::createView('newpost');
         Post::newPost();
+        NewPost::createView('newpost');
     }else{
         echo "Esegui il login per accedere a questa pagina!";
     }
 });
 
 Route::set('post',function(){
-    Post::createView('post');
     if (isset($_SESSION['email'])) {
         Post::newComment();
         Post::deletePost();
         Post::deleteComment();
     }
+    Post::createView('post');
 });
